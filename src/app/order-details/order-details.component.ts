533cdfb58cbed53c58b1a8aac94ceca39133ea42
borsim <bmiki97@gmail.com>
2023-03-07 20:27:12 +0100

    downloading data from db works, hooray. Now how do I refresh individual pages... and how do I upload it back

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DatabaseService } from '../services/database.service';
-import { Order } from '../orders';
-import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { Order, OrderInterface } from '../orders';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-order-details',
@@ -15,74 +18,48 @@ export class OrderDetailsComponent implements OnInit {
   openTab = 0;
   constructor(
     private route: ActivatedRoute,
-    private store: Firestore,
+    private store: AngularFirestore,
     private databaseService: DatabaseService
   ) {
     const routeParams = this.route.snapshot.paramMap;
-    const orderIdFromRoute = Number(routeParams.get('orderId'));
-    /*this.databaseService.databaseOrders.subscribe((dbOrders) => {
-      dbOrders.forEach((dbOrder) => {
-        this.orders = [];
-        this.orders.push(
-          new Order(
-            dbOrder.id,
-            dbOrder.name,
-            dbOrder.price,
-            dbOrder.description,
-            dbOrder.orderStatus,
-            dbOrder.icon,
-            dbOrder.customerName,
-            dbOrder.telephoneNumber,
-            dbOrder.email,
-            dbOrder.task,
-            dbOrder.deadline,
-            dbOrder.creationTime,
-            dbOrder.lastUpdatedTime,
-            dbOrder.returnedTime,
-            dbOrder.advancePayment,
-            dbOrder.notes,
-            dbOrder.doneTasks,
-            dbOrder.guarantee
-          )
-        );
-      });
-    });*/
-  }
+    const orderIdFromRoute = Number(routeParams.get('orderId')); // ?? routeParams.get('orderId') : ''
+    const oid: string = routeParams.get('orderId')!;
 
-  async getDocument(docId: string) {
-    const docRef = doc(this.store, 'Orders', docId);
-    const docSnap = await getDoc(docRef);
-    const docData = docSnap.data();
-    const newOrder = new Order(
-      docData.id,
-      docData.name,
-      docData.price,
-      docData.description,
-      docData.orderStatus,
-      docData.icon,
-      docData.customerName,
-      docData.telephoneNumber,
-      docData.email,
-      docData.task,
-      docData.deadline,
-      docData.creationTime,
-      docData.lastUpdatedTime,
-      docData.returnedTime,
-      docData.advancePayment,
-      docData.notes,
-      docData.doneTasks,
-      docData.guarantee
-    );
-    this.order = newOrder;
-    return newOrder;
+    let orderDoc = store.doc<OrderInterface>('Orders/' + oid);
+    let orderVC = orderDoc.valueChanges(); //{idField: 'id'});
+
+    orderVC.subscribe((dbOrder) => {
+      const nonNullOrder: Order =
+        dbOrder !== null
+          ? new Order(
+              Number(oid),
+              dbOrder!.name,
+              dbOrder!.price,
+              dbOrder!.description,
+              dbOrder!.orderStatus,
+              dbOrder!.icon,
+              dbOrder!.customerName,
+              dbOrder!.telephoneNumber,
+              dbOrder!.email,
+              dbOrder!.task,
+              dbOrder!.deadline,
+              dbOrder!.creationTime,
+              dbOrder!.lastUpdatedTime,
+              dbOrder!.returnedTime,
+              dbOrder!.advancePayment,
+              dbOrder!.notes,
+              dbOrder!.doneTasks,
+              dbOrder!.guarantee
+            )
+          : new Order(0);
+      this.order = nonNullOrder;
+    });
   }
 
   ngOnInit() {
     // First get the order id from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const oid: string = routeParams.get('orderId') ?? '';
-    alert(oid);
-    this.getDocument(oid);
     //const orderIdFromRoute = Number(routeParams.get('orderId'));
 
     // Find the order that correspond with the id provided in route.
