eb1d6bcc3aec1b6b04cb7845c025f9232d287886
Miklós Borsi <bmiki97@gmail.com>
2023-08-13 01:46:05 +0200

    we can do emails! Incredible! Also top bar and list view optimized for mobile view. Sort of. Added back link to settings page

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,65 +8,42 @@
  */
 
 import {onCall} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-
-//let { useremail, refreshtoken, clientid, clientsecret } = config().gmail;
-
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
+import * as admin from "firebase-admin";
+import * as nodemailer from "nodemailer";
+import {setGlobalOptions} from "firebase-functions/v2/options";
 
 admin.initializeApp();
+setGlobalOptions({region: "europe-west1"});
 
-/** defining and destructuring environments config for firebase functions */
-let { useremail, refreshtoken, clientid, clientsecret } = functions.config().gmail;
 
-/**create reusable transporter object using the gmail SMTP transport */
-let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        type: "OAuth2",
-        user: useremail,
-        clientId: clientid,
-        clientSecret: clientsecret,
-        refreshToken: refreshtoken,
-    },
+// create reusable transporter object using the gmail SMTP transport
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.USEREMAIL,
+    clientId: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    refreshToken: process.env.REFRESHTOKEN,
+  },
 });
 
+exports.sendemail = onCall((request) => {
+  // Data passed from the client.
+  const emailText = request.data().emailText;
+  const emailTo = request.data().emailTo;
+  const emailSubject = request.data().emailSubject;
 
-//our firebase cloud function
-exports.sendEmail = onCall((request: any) => {
-    // Data passed from the client.
-    const emailText = request.data.emailText;
-    const emailTo = request.data.emailTo;
-    const emailSubject = request.data.emailSubject;
-    const uid = request.auth.uid;
-    const name = request.auth.token.name || null;
-    const picture = request.auth.token.picture || null;
-    const email = request.auth.token.email || null;
-
-    // TODO: Replace the `from`, `html` and `subject` values
-    const mailOptions = {
-        from: "miklos.borsi.4@gmail.com",
-        to: emailTo,
-        subject: emailSubject,
-        html: emailText,
-    };
-
-// send mail with defined transport object
-return transporter.sendMail(mailOptions).catch((err: any)=>{
-        console.log(err);
-    });
+  const mailOptions = {
+    from: "miklos.borsi.4@gmail.com",
+    to: emailTo,
+    subject: emailSubject,
+    html: emailText,
+  };
+  // send mail with defined transport object
+  return transporter.sendMail(mailOptions).catch((err: string)=>{
+    console.log(err);
+  });
 });
-
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
