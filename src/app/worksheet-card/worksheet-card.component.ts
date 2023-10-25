08c43e3324db4a62f3d43d9cae0bd0c41b264843
unknown <bmiki97@gmail.com>
2023-10-26 00:22:10 +0200

    Fixed qrcode link on phone

--- src/app/worksheet-card/worksheet-card.component.ts
+++ src/app/worksheet-card/worksheet-card.component.ts
@@ -77,7 +77,7 @@ export class WorksheetCardComponent implements OnInit {
         this.timeSnapshot = moment();
         this.datetimeText = this.timeSnapshot.format('YYYY/MM/DD HH:mm');
         this.dateText = this.timeSnapshot.format('YYYY/MM/DD');
-        this.qrcodeString = 'https://www.oraszerviz-munkalap.firebaseapp.com/orders/'.concat(this.orderId); 
+        this.qrcodeString = 'https://oraszerviz-munkalap.firebaseapp.com/orders/'.concat(this.orderId); 
         this.wsFooterText = this.databaseService.currentUserConfig.value.worksheetCardFooterText;
         this.wsReceiptText = this.databaseService.currentUserConfig.value.worksheetCardReceiptText;
         this.afStorage.ref('/config/logo').getDownloadURL().subscribe((url) => {
